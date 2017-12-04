ed47d68c406d35808f4a4404ae1dc667f1775d36
Shaheena Qureshi <shaheenaqureshi@ShaheenasMBPro.hsd1.ga.comcast.net>
2017-12-04 17:16:28 -0500

    script file

--- assets/javascript/appscript.js
+++ assets/javascript/appscript.js
@@ -1,38 +1,43 @@
 
-
-
+//Global variables
+//Initial array populated with values to be displayed as buttons
 var characters = ["Superman", "Batman", "Wonder Woman", "The Joker", "Thor", "Loki", "Magneto", "Venom", "Spider Man", "Deadpool", "The Hulk", "Captain America"];
 
+//Function to create the buttons and display in the appropriate div in html
 		function makeButtons(){
-
+//Empty out the div from the previous display
 			$(".displaybuttons").empty();
-
+//Loop through the array and create a button for each index
 			for(var i = 0; i < characters.length; i++){
-				
+				//creating a button and adding class and css
 				var a = $("<button>");
 				a.addClass("btn btn-default");
 				a.css("background", "lightblue");
 				a.addClass("character-button");
 				a.attr("data-name", characters[i]);
 				a.text(characters[i]);
+				//displaying the created buttons in a location on the html page
 				$(".displaybuttons").append(a);
 			}
 		}
-
+//Function to get the information from the giphy app and display it on page
 		function displayImages(){
+			//Empty out the div from the previous display of gifs
 			$(".display-images").empty();
-			
+			//creating a variable that will hold the name,from the array[i], of the button
 			var character = $(this).attr("data-name");
+			//Putting together the url for the query with the name variable
 			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         character + "&api_key=hcPTEdeIxgj1yAl46R2v9DwCoab5N6R0&limit=10";
-
+//Calling the ajax method to get the data from the api
         $.ajax({
         	url: queryURL,
         	method: "GET"
         }).done(function(response){
         	
         	var results = response.data;
-        	
+  //looping through to create a div which holds the rating and image data
+  //for the requested query
         	for(var i = 0; i < results.length; i++){
         	
         	var characterDiv = $("<div>");
@@ -62,14 +67,14 @@ var characters = ["Superman", "Batman", "Wonder Woman", "The Joker", "Thor", "Lo
 
         	characterDiv.append(image);
 
-
+//Displaying in the div location on the html page
         	$(".display-images").append(characterDiv);
         	
         	}
         });
 
 		}
-
+//Click on the gif images in the document and the function will pause or animate the gif
 		$(document).on("click", ".imgGif", function(){
 			var state = $(this).attr("data-state");
 
@@ -87,20 +92,21 @@ var characters = ["Superman", "Batman", "Wonder Woman", "The Joker", "Thor", "Lo
 		});
 
 			
-
+//Input submit button to get the value from the input box and add it to the array
 		
 		$("#add-character").on("click", function(e){
 			e.preventDefault();
 
 			var item = $("#character-input").val().trim();
 			characters.push(item);
-
+//call the function to make new buttons from array
 			makeButtons();
+			//clears the input box afterwards
 			$("#character-input").val("");
 		});
 
 
-
+//Listens for click on the buttons on the document
 		$(document).on("click", ".character-button", displayImages);
 
 		makeButtons();
